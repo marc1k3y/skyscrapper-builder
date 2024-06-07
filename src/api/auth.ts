623f50c618a08da1d9afa9f1c662da85d005a156
marc1k3y <marc1k3y@gmail.com>
2024-06-07 20:00:40 +0300

    test

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,8 +4,7 @@ import axios from "axios";
 
 export const AuthInitAPI = async (tma: string) => {
   try {
-    const { data } = await axios.get("https://api.tmaserver.online/api/auth/init", { headers: { "custom-auth": `tma ${tma}` } });
-    // const { data } = await axios.get("https://api.tmaserver.online/api/auth/init");
+    const { data } = await axios.get("https://api.tmaserver.online/api/auth/init", { headers: { "Authorization": `tma ${tma}` } });
     return data;
   } catch (e) {
     return console.error(e);
