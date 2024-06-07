4f3d33e24789e72a8a02ade92981a2995b98e000
marc1k3y <marc1k3y@gmail.com>
2024-06-07 14:32:59 +0300

    test

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const AuthInitAPI = async (tma: string) => {
   try {
     // const { data } = await $authHost.get("auth/init");
-    const { data } = await axios.get("auth/init", { headers: { "Authorization": `tma ${tma}` } });
+    const { data } = await axios.get("https://skyb-node.onrender.com:10000/api/auth/init", { headers: { "Authorization": `tma ${tma}` } });
     return data;
   } catch (e) {
     return console.error(e);
