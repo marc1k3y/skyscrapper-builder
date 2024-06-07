a8fb511131481ff8fc6f3653c7ef56d68f33b59f
marc1k3y <marc1k3y@gmail.com>
2024-06-07 12:37:53 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useInitData, useInitDataRaw } from '@tma.js/sdk-react'
+import { retrieveLaunchParams, useInitData, useInitDataRaw } from "@tma.js/sdk-react";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const initData = useInitData();
-  const initDataRaw = useInitDataRaw();
+  const initDataHook = useInitData();
+  const initDataRawHook = useInitDataRaw();
+  const { initData, initDataRaw } = retrieveLaunchParams()
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        <div>initData: {JSON.stringify(initData)}</div>
-        <div>initDataRaw: {JSON.stringify(initDataRaw)}</div>
-      </p>
-    </>
+    <p className="read-the-docs">
+      <div>initData: {JSON.stringify(initData)}</div>
+      <div>initDataRaw: {JSON.stringify(initDataRaw)}</div>
+      <div>initDataHook: {JSON.stringify(initDataHook)}</div>
+      <div>initDataRawHook: {JSON.stringify(initDataRawHook)}</div>
+    </p>
   )
 }
 
